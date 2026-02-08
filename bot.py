f6053ff7fc1cff67509db263f6314ddd36ae08a8
gariy1977 <100241988+gariy1977@users.noreply.github.com>
2026-02-08 16:20:02 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -205,7 +205,6 @@ async def on_startup(app):
 
 async def on_shutdown(app):
     await bot.delete_webhook()
-    await storage.close()  # wait_closed() удаляем
     await bot.session.close()
     print("Webhook видалено, з’єднання закрито ✅")
 
