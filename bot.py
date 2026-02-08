a2680befc4b79cb445fb7c222b7248c33fc16c0c
gariy1977 <100241988+gariy1977@users.noreply.github.com>
2026-02-08 13:56:14 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -18,7 +18,7 @@ CHANNEL_ID = os.getenv("CHANNEL_ID") or "-1003571651319"
 REDIS_URL = os.getenv("REDIS_URL") or "redis://default:63b9cac5873c4abead1146f565ef7dff@fly-ra-redis.upstash.io:6379"
 
 # ===== REDIS STORAGE =====
-redis_client = Redis.from_url(REDIS_URL)
+redis_client = Redis.from_url(REDIS_URL, decode_responses=True, ssl=True)
 storage = RedisStorage(redis=redis_client)
 
 bot = Bot(token=TOKEN)
